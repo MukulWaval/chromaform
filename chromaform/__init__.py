d4c95b2783ac2941159c058c2aa248aa1a048cdc
MukulWaval <mukulwaval2000@gmail.com>
2025-03-18 22:32:29 +0530

    Refactor logging setup and define version constants

--- chromaform/__init__.py
+++ chromaform/__init__.py
@@ -1,11 +1,12 @@
 import logging
-
-# Setup logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
 # Import key functions/classes
 from .module1 import function1
 from .module2 import Class1
 
-__version__ = "0.1.0"
+# Setup logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__version__ = "0.1.0"  # Define the version
+VERSION = __version__
+VERSION_SHORT = __version__.split(".")[0]
 __all__ = ["function1", "Class1"]
